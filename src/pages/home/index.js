c36af8db9cf0004f2f9cfbf623f3e9e01b009b39
siccoo <obichilaka@gmail.com>
2021-03-11 17:07:00 +0100

    updates on Home.js & Home.css

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Topbar from "../../components/navbar/index";
 
+import Mobile from "../../images/Phones.svg"
+
 const Home = () => {
     return (
         <div>
             <section>
                 <Topbar />
+            </section>
+            <section className="hero1">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 align-self-center">
+
+                        </div>
+                        <div className="col-md-6">
+                            <img src={Mobile} alt="alternative" className="hero1-img" />
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     )

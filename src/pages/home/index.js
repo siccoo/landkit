cc1ad2e8365e114c45983e87eed853eb12cf2b5e
siccoo <obichilaka@gmail.com>
2021-03-11 20:04:25 +0100

    updates on hero2

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Topbar from "../../components/navbar/index";
 import "./style.css";
 
-import Mobile from "../../images/Phones.svg"
+import Mobile from "../../images/Phones.svg";
+import Staff from "../../images/CompImg.svg";
+import RealTime from "../../images/Converse.svg"
 
 const Home = () => {
     return (
@@ -10,6 +12,7 @@ const Home = () => {
             <section>
                 <Topbar />
             </section>
+
             <section className="hero1">
                 <div className="container">
                     <div className="row">
@@ -25,6 +28,40 @@ const Home = () => {
                         </div>
                         <div className="col-md-6">
                             <img src={Mobile} alt="alternative" className="hero1-img" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="hero2">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <img src={Staff} alt="alternative" className="hero2-img" />
+                        </div>
+                        <div className="col-md-6 align-self-center">
+                                <h3>Keep in touch while traveling the world.</h3>
+                                <p>Forward-thinking businesses use Timekit to imagine, <br/>design and manage the perfect scheduling flows to <br />imagine, design and manage.</p>
+                                <div className="input-group">
+                                    {/* <input type="email" name="email" id="email" className="" placeholder="(123) 4567-8901" /> */}
+                                    <span className="input-group-btn px-3">
+                                        <button className="btn" type="submit">View show list</button>
+                                    </span>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="hero3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 align-self-center">
+                                <h3>Chat with your friends while watching together.</h3>
+                                <p>Forward-thinking businesses use Timekit to imagine, <br/>design and manage the perfect scheduling flows to <br />imagine, design and manage.</p>
+                        </div>
+                        <div className="col-md-6">
+                            <img src={RealTime} alt="alternative" className="hero-img" />
                         </div>
                     </div>
                 </div>
